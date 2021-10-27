2a2be86aea9e5ea04ffc9d4bfbc87abb3ee66a4f
henning mueller <mail@nning.io>
2021-10-27 13:08:38 +0200

    Add option to show app ID

--- cmd/list-proton-versions/main.go
+++ cmd/list-proton-versions/main.go
@@ -13,11 +13,13 @@ func main() {
 	var all bool
 	var ignoreCache bool
 	var jsonOutput bool
+	var showAppId bool
 	var user string
 
 	flag.BoolVar(&all, "a", false, "List both installed and non-installed games")
+	flag.BoolVar(&ignoreCache, "c", false, "Ignore app ID/name cache")
 	flag.BoolVar(&jsonOutput, "j", false, "Output JSON (implies -a)")
-	flag.BoolVar(&ignoreCache, "i", false, "Ignore app id/name cache")
+	flag.BoolVar(&showAppId, "i", false, "Show app ID")
 	flag.StringVar(&user, "u", "", "Steam user name (or SteamID3)")
 	flag.Parse()
 
@@ -34,6 +36,9 @@ func main() {
 			for _, game := range games.Sort() {
 				if all || games[game].IsInstalled {
 					fmt.Print("\t" + game)
+					if showAppId {
+						fmt.Print(" (" + games[game].Id + ")")
+					}
 					if !games[game].IsInstalled {
 						fmt.Print(" [NOT INSTALLED]")
 					}
