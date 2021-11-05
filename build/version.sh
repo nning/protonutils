fbd979c67bbb1b1c8ce2ce45630ecfa5a187b41d
henning mueller <mail@nning.io>
2021-11-05 16:35:11 +0100

    Work-around strange behaviour of git

--- build/version.sh
+++ build/version.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-version=$(git name-rev --tags --name-only $(git rev-parse HEAD))
+version=$(git name-rev --tags --name-only HEAD | cut -d^ -f1)
 [ $version = "undefined" ] && version=$(git rev-parse --short HEAD)
 echo $version
