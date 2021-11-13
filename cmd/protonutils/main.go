098ff752c7b33faef4ef21b5e55c91c7adedbda0
henning mueller <mail@nning.io>
2021-11-13 19:25:21 +0100

    Generate man pages

--- cmd/protonutils/main.go
+++ cmd/protonutils/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
 )
 
 var rootCmd = &cobra.Command{
@@ -12,6 +13,8 @@ var rootCmd = &cobra.Command{
 	Short: "Utilities for using the Proton compatibility tool",
 	Long:  "protonutils is a CLI tool that provides different utilities to make using the Proton compatibility tool more easy.",
 }
+
+var manDir string
 
 func exitOnError(e error, a ...string) {
 	if e != nil {
@@ -25,7 +28,29 @@ func exitOnError(e error, a ...string) {
 	}
 }
 
+func init() {
+	rootCmd.Flags().StringVarP(&manDir, "generate-man", "m", "", "")
+	rootCmd.Flags().MarkHidden("generate-man")
+	rootCmd.MarkFlagDirname("generate-man")
+}
+
 func main() {
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	rootCmd.ParseFlags(os.Args)
+
+	if manDir != "" {
+		header := &doc.GenManHeader{
+			Title:   "PROTONUTILS",
+			Section: "1",
+		}
+		err := os.MkdirAll(manDir, 0700)
+		exitOnError(err)
+
+		err = doc.GenManTree(rootCmd, header, manDir)
+		exitOnError(err)
+
+		return
+	}
+
 	rootCmd.Execute()
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/MrWaggel/gosteamconv v0.0.0-20190214041723-97e1fbb6de26
 	github.com/andygrunwald/vdf v1.0.0
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/dustin/go-humanize v1.0.0
 	github.com/spf13/cobra v1.2.1
 	github.com/stretchr/testify v1.7.0
