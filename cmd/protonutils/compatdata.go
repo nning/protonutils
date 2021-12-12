c61501aad80365a17bc23e05c9bc558697a6419d
henning mueller <mail@nning.io>
2021-12-12 19:29:28 +0100

    Add verbose option to `compatdata open/path` (that shows game name)

--- cmd/protonutils/compatdata.go
+++ cmd/protonutils/compatdata.go
@@ -30,24 +30,30 @@ var compatdataOpenCmd = &cobra.Command{
 	Run:   compatdataOpen,
 }
 
+var verbose bool
+
 func init() {
 	rootCmd.AddCommand(compatdataCmd)
 
 	compatdataCmd.AddCommand(compatdataPathCmd)
 	compatdataPathCmd.Flags().StringVarP(&user, "user", "u", "", "Steam user name (or SteamID3)")
 	compatdataPathCmd.Flags().BoolVarP(&ignoreCache, "ignore-cache", "c", false, "Ignore app ID/name cache")
+	compatdataPathCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Show app name")
 
 	compatdataCmd.AddCommand(compatdataOpenCmd)
 	compatdataOpenCmd.Flags().StringVarP(&user, "user", "u", "", "Steam user name (or SteamID3)")
 	compatdataOpenCmd.Flags().BoolVarP(&ignoreCache, "ignore-cache", "c", false, "Ignore app ID/name cache")
+	compatdataOpenCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Show app name")
 }
 
-func getPath(idOrName string) string {
+func getPath(idOrName string) (string, string) {
 	s, err := steam.New(user, ignoreCache)
 	exitOnError(err)
 
 	p, err := s.GetLibraryPath(idOrName)
 	exitOnError(err)
+
+	var n string
 
 	if p == "" {
 		err = s.ReadCompatToolVersions()
@@ -61,6 +67,7 @@ func getPath(idOrName string) string {
 
 				if a == b || strings.HasPrefix(a, b) && game.IsInstalled {
 					id = game.ID
+					n = name
 					break
 				}
 			}
@@ -77,16 +84,26 @@ func getPath(idOrName string) string {
 		}
 	}
 
-	return path.Join(p, "steamapps", "compatdata", idOrName)
+	return path.Join(p, "steamapps", "compatdata", idOrName), n
 }
 
 func compatdataPath(cmd *cobra.Command, args []string) {
-	p := getPath(args[0])
+	p, n := getPath(args[0])
+
+	if verbose {
+		fmt.Println(n)
+	}
+
 	fmt.Println(p)
 }
 
 func compatdataOpen(cmd *cobra.Command, args []string) {
-	p := getPath(args[0])
+	p, n := getPath(args[0])
+
+	if verbose {
+		fmt.Println(n)
+	}
+
 	_, err := exec.Command("xdg-open", p).Output()
 	exitOnError(err)
 }
