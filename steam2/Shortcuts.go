1252e294664d6c74c53b827f60e4ff21adfeefd2
henning mueller <mail@nning.io>
2022-02-04 19:25:55 +0100

    Ignore if shortcuts VDF is not existing

--- steam2/Shortcuts.go
+++ steam2/Shortcuts.go
@@ -2,6 +2,7 @@ package steam2
 
 import (
 	"io/ioutil"
+	"os"
 	"path"
 )
 
@@ -11,7 +12,7 @@ const InnerOffsetShortcuts = 1
 func (s *Steam) initShortcuts() error {
 	p := path.Join(s.Root, "userdata", s.UID, "config", "shortcuts.vdf")
 	in, err := ioutil.ReadFile(p)
-	if err != nil {
+	if err != nil && !os.IsNotExist(err) {
 		return err
 	}
 
